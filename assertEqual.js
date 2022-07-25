f1e9aef3c304b882386751f8c7f06d0f9a1e2ea6
Colton <coltonbitz@gmail.com>
2022-07-25 22:13:08 +0000

    Setup assertEqual.js and tested

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅✅✅: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    return console.log(`🛑🛑🛑: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Mountain Bike", "Mountain Bike");
+assertEqual(1, 1);
+assertEqual(1, 2);
