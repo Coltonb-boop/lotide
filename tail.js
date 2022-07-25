67b54e1fcc02b7239c5c18b61bac155b40ad33b6
Colton <coltonbitz@gmail.com>
2022-07-25 22:57:33 +0000

    added test cases to tail.js

--- tail.js
+++ tail.js
@@ -11,12 +11,17 @@ const tail = (array) => {
     return array.slice(1);
   }
   return undefined;
-}
+};
 
 // Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 const result = tail(words);
-
 assertEqual(result.length, 2);
 assertEqual(result[0], 'Lighthouse');
 assertEqual(result[1], 'Labs');
+
+
+let words2 = [];
+let result2 = tail(words2);
+assertEqual(result2.length, 0);
+assertEqual(result2[0], undefined);
