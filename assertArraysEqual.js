a26b9a0d6c1f3cb87e3d59d5a5e78b3f6092ad27
Colton <coltonbitz@gmail.com>
2022-07-26 23:17:25 +0000

    completed assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,20 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    return console.log(`✅✅✅: ${actual} === ${expected}`);
+  } 
+  return console.log(`🛑🛑🛑: ${actual} !== ${expected}`);
+};
+
+const eqArrays = (first, second) => {
+  for (let i = 0; i < first.length; i++) {
+    if (first[i] !== second[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(['cheese', 'buns', 'are', 'tasty'], ['cheese', 'buns', 'are', 'tasty']);
+assertArraysEqual(['cheese', 'buns', 'are', 'tasty'], ['buns', 'are', 'tasty']);
--- eqArrays.js
+++ eqArrays.js
@@ -24,3 +24,4 @@ eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
+assertEqual(eqArrays([3, 2, 1], ['3', 2, 1]), false); // => should PASS
