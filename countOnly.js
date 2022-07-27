71b1e6ac19701061b86b172aab829d01c5039a8d
Colton <coltonbitz@gmail.com>
2022-07-27 20:39:41 +0000

    completed countOnly.js

---
+++ countOnly.js
@@ -0,0 +1,44 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅✅✅: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    return console.log(`🛑🛑🛑: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+const countOnly = (allItems, itemsToCount) => {
+  // create blank object to store return
+  let output = {};
+
+  // loop through allItems and compare to itemsToCount
+  // if it's being counted, add it to our blank object
+  for (let item of allItems) {
+    if (itemsToCount[item]) {
+      !output[item] ? output[item] = 1 : output[item]++;
+    }
+  }
+
+  // return our blank object
+  return output;
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
