dbe4ce766c21da67ecc8e39a6e1ee2eb0bea7a95
Colton <coltonbitz@gmail.com>
2022-07-27 20:50:15 +0000

    added comments countLetters.js

--- countLetters.js
+++ countLetters.js
@@ -15,7 +15,8 @@ const countLetters = (sentenceToCount) => {
   // loop through allItems and compare to itemsToCount
   // if it's being counted, add it to our blank object
   for (let letter of sentenceToCount) {
-    if (letter === ' ') continue;
+    if (letter === ' ') continue; // continue onto next letter if this is a space
+    // could also split(' ').join('') to get a similar affect
     !output[letter] ? output[letter] = 1 : output[letter]++;
   }
   console.log(output);
