7ccca4c883faf4d31cd454b32a49602cb7b4a91a
Coltonb-boop <coltonbitz@gmail.com>
2022-08-01 14:01:11 -0600

    created a master index.js that exports all of our lotide functions into one export!

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,32 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual(middle([1]), [])
-assertArraysEqual(middle([1, 2]), [])
-assertArraysEqual(middle([1, 2, 3]), [2])
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+describe('#Middle Tests', () => {
+  it('should return [] when given [1]', () => {
+    const words = [1];
+    const result = middle(words);
+    assert.deepEqual(result, []);
+  });
+  it('should return [] when given [1, 2]', () => {
+    const words = [1, 2];
+    const result = middle(words);
+    assert.deepEqual(result, []);
+  });
+  it('should return [2] when given [1, 2, 3]', () => {
+    const words = [1, 2, 3];
+    const result = middle(words);
+    assert.deepEqual(result, [2]);
+  });
+  it('should return [2, 3] when given [1, 2, 3, 4]', () => {
+    const words = [1, 2, 3, 4];
+    const result = middle(words);
+    assert.deepEqual(result, [2, 3]);
+  });
+});
+
+// assertArraysEqual(middle([1]), [])
+// assertArraysEqual(middle([1, 2]), [])
+// assertArraysEqual(middle([1, 2, 3]), [2])
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
